2758b5bb7284c9c35cee0e13fd2afeb4860db0bf
Brian Johnson <mbjohn@duke.edu>
2026-02-17 12:57:35 -0500

    more copyright changes

--- cli.py
+++ cli.py
@@ -1,6 +1,6 @@
 # ---------------------------------------------------------------------------
 # NET Bible (2nd Ed) Builder
-# Copyright (C) 2026 Brian Johnson
+# Copyright (C) 2026 The net-bible-builder Authors
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
